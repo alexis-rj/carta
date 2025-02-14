7b22d24053c024efacf970446c54314ae3f3dec6
alexis-rj <alex9702009@gmail.com>
2025-02-13 22:34:15 -0600

    Archivos

---
+++ main.js
@@ -0,0 +1,7 @@
+const sobre = document.querySelector('.sobre');
+const tapa = document.querySelector('.tapa');
+const carta = document.querySelector('.carta');
+
+sobre.addEventListener('click', () => {
+    sobre.classList.toggle('abierto');
+});
